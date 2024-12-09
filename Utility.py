82d109b826f5f295133c725839ae4466996bfbbf
Piadave <pdave4@student.gsu.edu>
2024-12-09 15:26:32 -0500

    Update Utility.py

--- Utility.py
+++ Utility.py
@@ -1,1 +1,27 @@
 #This is the file for all functions
+
+#suppose the scraped data is stored in "data"
+#user choice of genre is "genre"
+
+#Function 1
+#Function 2
+def func2:
+  positive_reviews = []
+  for i in data:
+      if i['review score']>=6:
+          positive_reviews.appead(i)
+  return positive_reviews
+
+#Function 3
+def func3:
+  topfive = []
+  n = 5
+  for i in positive_reviews:
+      while n > 0:
+        max = max(positive_reviews, 'rating')
+        topfive.append(max)
+        data.remove(max)
+        n = n - 1
+      return topfive 
+      
+      
