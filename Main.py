9562f861e342290fc7f71ea458ed1f2403f5a6fc
Piadave <pdave4@student.gsu.edu>
2024-12-13 14:38:03 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,15 +1,19 @@
 #This is the file for main code
+
+#Import the file with the functions
+import Utility.py
+
 print("Let's find a movie to watch!")
 
-genres = ["Action", "Comedy", "Drama", "Mystery/Thriller", "Horror"]
+allgenres = ["Action", "Comedy", "Drama", "Mystery/Thriller", "Horror"]
 print("Please choose a genre:")
 for i in range(len(genres)):
    print(str(i + 1) + ". " + genres[i])
 
 genre_input = int(input("\nEnter the number of the genre you're interested in: "))
-selected_genre = genres[genre_input - 1]
+user_genre = genres[genre_input - 1]
 
-print("\nYou selected the " + selected_genre + " genre.")
+print("\nYou selected the " + user_genre + " genre.")
 print("Do you want to see:")
 print("1. Most Recent Movies")
 print("2. Top Rated Movies")
@@ -17,8 +21,8 @@ print("2. Top Rated Movies")
 choice = int(input("\nEnter 1 or 2: "))
 
 if choice == 1:
-   display_most_recent_movies(movies_data, selected_genre)
+   func1()
 elif choice == 2:
-   display_top_rated_movies(movies_data, selected_genre)
+   func2()
 else:
    print("Invalid choice, please restart the program.")
